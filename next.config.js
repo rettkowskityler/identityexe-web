f5a3877bd231af68d3a1d30e8b298c96e1a93a2b
Tyler Rettkowski <rettkowski.tyler@gmail.com>
2026-07-19 11:54:33 -0700

    feat: LI tracking slug

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     allowedDevOrigins: ['192.168.0.140', 'localhost:3000'],
   },
   typescript: { ignoreBuildErrors: true },
+  async rewrites() {
+    return [
+      {
+        source: '/blog/:slug/li-:date',
+        destination: '/blog/:slug',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
